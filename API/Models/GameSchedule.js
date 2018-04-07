4a61bfc71fdd078022134be5a90c1589c146129d
ogonzalez29 <oscar.d.gonzalez@gmail.com>
2018-04-07 14:12:58 -0500

    activated model data generation in mongoDB on deployment

--- API/Models/GameSchedule.js
+++ API/Models/GameSchedule.js
@@ -9,16 +9,16 @@ var schema = new mongoose.Schema({
 
 var GameSchedule = mongoose.model('GameSchedule', schema);
 
-// var doc = new GameSchedule({
-// 	date: "2018-02-07T14:30:00Z",
-//     opponent: "Portland",
-//     hasBeenPlayed: true,
-//     isWinner: true,
-//     score: "110-130"
-// })
+var doc = new GameSchedule({
+	date: "2018-02-07T14:30:00Z",
+    opponent: "Portland",
+    hasBeenPlayed: true,
+    isWinner: true,
+    score: "110-130"
+})
 
-// doc.save(function(err){
-// 	if (err) return handleError(err);
-// })
+doc.save(function(err){
+	if (err) return handleError(err);
+})
 
 module.exports = GameSchedule;
--- API/Models/TeamInfo.js
+++ API/Models/TeamInfo.js
@@ -6,13 +6,13 @@ var schema = new mongoose.Schema({
 
 var TeamInfo = mongoose.model('TeamInfo', schema);
 
-// var doc = new TeamInfo({
-// 	name: "Sacramento Kings",
-// 	description: "The Sacramento Kings are an American professional basketball team based in Sacramento, California. The Kings compete in the National Basketball Association as a member of the Western Conference Pacific Division."
-// })
+var doc = new TeamInfo({
+	name: "Sacramento Kings",
+	description: "The Sacramento Kings are an American professional basketball team based in Sacramento, California. The Kings compete in the National Basketball Association as a member of the Western Conference Pacific Division."
+})
 
-// doc.save(function(err){
-// 	if (err) return handleError(err);
-// })
+doc.save(function(err){
+	if (err) return handleError(err);
+})
 
 module.exports = TeamInfo;
