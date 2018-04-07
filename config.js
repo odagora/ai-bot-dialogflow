3752072859b6818abf5df09d2ef2b386ac5c4188
ogonzalez29 <oscar.d.gonzalez@gmail.com>
2018-04-07 14:10:25 -0500

    added mlab database config for heroku

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports =
 {
-dbUrl: "mongodb://ogonzalez29:password@ds127139.mlab.com:27139/nba"
+dbUrl: process.env.MONGODB_URI || "mongodb://ogonzalez29:password@ds127139.mlab.com:27139/nba"
 }
